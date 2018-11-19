5e019ae68ad3511c924554f8414958084acac73c
Harry Zhang <harryzhangbruins@gmail.com>
2018-11-19 11:45:37 -0800

    Bouding Box overlay
    use bounding box instead of mapped overlay

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 //"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files
+test = true
 
 var slide_url  = image_data['patient'][0]['images']
 var mask_url = image_data['mask'][0]['path'] //use ['mask'] in next step
@@ -40,10 +41,8 @@ var tour = new Tour({
 App = {
 	init:function(){
 
-
-
 		// openSeadragon object;
-		var viewer = new OpenSeadragon({
+		viewer = new OpenSeadragon({
 		    id:  "view",
 		    tileSources: [slide_url],
 		    prefixUrl: "static/images/",
@@ -67,9 +66,9 @@ App = {
 		// levels.  This is a hack: we can't configure the minLevel via
 		// OpenSeadragon configuration options when the viewer is created
 		// from DZI XML.
-		viewer.addHandler("open", function() {
-			viewer.source.minLevel = 8;
-		});
+		// viewer.addHandler("open", function() {
+		// 	 viewer.source.minLevel = 8;
+		// });
 
 
 		viewer.scalebar({
@@ -99,201 +98,29 @@ App = {
 	    //initialize selection
 	    var selection = viewer.selection(options);
 
-	    // initialize drawing overlay
-	    var drawing_overlay = viewer.fabricjsOverlay(options);
+		//bounding box with fabric.js
 
-	    var canvas = drawing_overlay.fabricCanvas('c',{
-	    	isDrawingMode:true
-	    });
+		var drawRect = function(e,i) {
+			if (e.label==1 || e.label==2){
+				if(e.label==1){
+				color_value="red";
+				}
+				else if(e.label==2){
+					color_value="yellow";
+				}
+				var boundBoxRect = viewer.viewport.imageToViewportRectangle(e.x,e.y,e.delta_x,e.delta_y);
+				var elt = document.createElement('div');
+				elt.id="bounding-box-" + i;
+				elt.className='runtime-overlay'
+            	elt.style.cssText= "border: 2px solid  " + color_value
+				viewer.addOverlay({
+					element:elt,
+					location: boundBoxRect
+				});
+			}
 
-	  	fabric.Object.prototype.transparentCorners = false;
-	  	
-	  	var drawingModeEl = $('#drawing-mode'),
-	      	drawingOptionsEl = $('#drawing-mode-options'),
-	      	drawingColorEl = $('#drawing-color'),
-	      	drawingShadowColorEl = $('#drawing-shadow-color'),
-	      	drawingLineWidthEl = $('#drawing-line-width'),
-	      	drawingShadowWidth = $('#drawing-shadow-width'),
-	      	drawingShadowOffset = $('#drawing-shadow-offset'),
-	      	clearEl = $('#clear-canvas');
+    	};
 
-	  	clearEl.click(function() { canvas.clear() });
-
-	  	drawingModeEl.click(function() {
-
-	    	canvas.isDrawingMode = !canvas.isDrawingMode;
-	    	if (canvas.isDrawingMode) {
-	    	canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
-	    	viewer.setMouseNavEnabled(false);
-	    	viewer.outerTracker.setTracking(false);
-	      	drawingModeEl.text('Cancel drawing mode');
-	      	drawingOptionsEl.show();
-	    	}
-	    	else {
-	    	viewer.setMouseNavEnabled(true);
-	    	viewer.outerTracker.setTracking(true);
-	      	drawingModeEl.text('Enter drawing mode');
-	      	drawingOptionsEl.hide();
-
-	    	}
-	  	});
-	                            
-	  	if (fabric.PatternBrush) {
-	    	var vLinePatternBrush = new fabric.PatternBrush(canvas);
-	    	vLinePatternBrush.getPatternSrc = function() {
-
-		      	var patternCanvas = fabric.document.createElement('canvas');
-		      	patternCanvas.width = patternCanvas.height = 10;
-		      	var ctx = patternCanvas.getContext('2d');
-
-		      	ctx.strokeStyle = this.color;
-		      	ctx.lineWidth = 5;
-		      	ctx.beginPath();
-		      	ctx.moveTo(0, 5);
-		      	ctx.lineTo(10, 5);
-		      	ctx.closePath();
-		      	ctx.stroke();
-
-		      	return patternCanvas;
-	    	};
-
-	    	var hLinePatternBrush = new fabric.PatternBrush(canvas);
-	    	hLinePatternBrush.getPatternSrc = function() {
-
-		      	var patternCanvas = fabric.document.createElement('canvas');
-		      	patternCanvas.width = patternCanvas.height = 10;
-		      	var ctx = patternCanvas.getContext('2d');
-
-		      	ctx.strokeStyle = this.color;
-		      	ctx.lineWidth = 5;
-		      	ctx.beginPath();
-		      	ctx.moveTo(5, 0);
-		      	ctx.lineTo(5, 10);
-		      	ctx.closePath();
-		      	ctx.stroke();
-
-		      	return patternCanvas;
-	    	};
-
-		    var squarePatternBrush = new fabric.PatternBrush(canvas);
-		    squarePatternBrush.getPatternSrc = function() {
-
-		      	var squareWidth = 10, squareDistance = 2;
-
-		      	var patternCanvas = fabric.document.createElement('canvas');
-		      	patternCanvas.width = patternCanvas.height = squareWidth + squareDistance;
-		      	var ctx = patternCanvas.getContext('2d');
-
-		      	ctx.fillStyle = this.color;
-		      	ctx.fillRect(0, 0, squareWidth, squareWidth);
-
-		      	return patternCanvas;
-		    };
-
-		    var diamondPatternBrush = new fabric.PatternBrush(canvas);
-		    diamondPatternBrush.getPatternSrc = function() {
-
-		      	var squareWidth = 10, squareDistance = 5;
-		      	var patternCanvas = fabric.document.createElement('canvas');
-		      	var rect = new fabric.Rect({
-		        	width: squareWidth,
-		        	height: squareWidth,
-		        	angle: 45,
-		        	fill: this.color
-		      	});
-
-		      	var canvasWidth = rect.getBoundingRect().width;
-
-		      	patternCanvas.width = patternCanvas.height = canvasWidth + squareDistance;
-		      	rect.set({ left: canvasWidth / 2, top: canvasWidth / 2 });
-
-		      	var ctx = patternCanvas.getContext('2d');
-		      	rect.render(ctx);
-
-		      	return patternCanvas;
-		    };
-
-		    var img = new Image();
-		    img.src = 'static/asset/honey_im_subtle.png';
-
-		    var texturePatternBrush = new fabric.PatternBrush(canvas);
-		    texturePatternBrush.source = img;
-	  	}
-
-	  	$('#drawing-mode-selector').change(function() {
-
-		    if (this.value === 'hline') {
-		      canvas.freeDrawingBrush = vLinePatternBrush;
-		    }
-		    else if (this.value === 'vline') {
-		      canvas.freeDrawingBrush = hLinePatternBrush;
-		    }
-		    else if (this.value === 'square') {
-		      canvas.freeDrawingBrush = squarePatternBrush;
-		    }
-		    else if (this.value === 'diamond') {
-		      canvas.freeDrawingBrush = diamondPatternBrush;
-		    }
-		    else if (this.value === 'texture') {
-		      canvas.freeDrawingBrush = texturePatternBrush;
-		    }
-		    else if (this.value === 'Pencil'){
-		      canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
-		    }
-
-		    if (canvas.freeDrawingBrush) {
-		      	canvas.freeDrawingBrush.color = drawingColorEl.value;
-		      	canvas.freeDrawingBrush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
-		      	canvas.freeDrawingBrush.shadow = new fabric.Shadow({
-			        blur: parseInt(drawingShadowWidth.value, 10) || 0,
-			        offsetX: 0,
-			        offsetY: 0,
-			        affectStroke: true,
-			        color: drawingShadowColorEl.value,
-		      	});
-		    }
-	  	});
-
-	  	drawingColorEl.change(function() {
-	    	canvas.freeDrawingBrush.color = this.value;
-	  	});
-
-	  	drawingShadowColorEl.change(function() {
-	    	canvas.freeDrawingBrush.shadow.color = this.value;
-	  	});
-
-	  	drawingLineWidthEl.change(function() {
-	    	canvas.freeDrawingBrush.width = parseInt(this.value, 10) || 1;
-	    	this.previousSibling.innerHTML = this.value;
-	  	});
-
-	  	drawingShadowWidth.change(function() {
-	    	canvas.freeDrawingBrush.shadow.blur = parseInt(this.value, 10) || 0;
-	    	this.previousSibling.innerHTML = this.value;
-	  	});
-
-	  	drawingShadowOffset.change(function() {
-	    	canvas.freeDrawingBrush.shadow.offsetX = parseInt(this.value, 10) || 0;
-	    	canvas.freeDrawingBrush.shadow.offsetY = parseInt(this.value, 10) || 0;
-	    	this.previousSibling.innerHTML = this.value;
-	  	});
-
-	  	if (canvas.freeDrawingBrush) {
-	    	canvas.freeDrawingBrush.color = drawingColorEl.value;
-	    	canvas.freeDrawingBrush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
-	    	canvas.freeDrawingBrush.shadow = new fabric.Shadow({
-		      	blur: parseInt(drawingShadowWidth.value, 10) || 0,
-		      	offsetX: 0,
-		      	offsetY: 0,
-		      	affectStroke: true,
-		      	color: drawingShadowColorEl.value,
-	    	});
-	  	}
-
-	  	$(window).resize(function(){
-	  		drawing_overlay.resize();
-	  		drawing_overlay.resizecanvas();
-	  	})
 
 
 		$('#toggle-overlay').click(function(){
@@ -304,10 +131,15 @@ App = {
 
 
 			if ($('#toggle-overlay').html()=="Display Prediction Mask") {
-				if(Object.values(image_data['mask']).indexOf(mask_url) > -1){
+				if(test==true){
 					$('#toggle-overlay').html('Close Prediction Mask');
 					$('#toggle-overlay').removeClass('uk-button-danger');
 					$('#toggle-overlay').addClass('uk-button-primary');
+					for(var i=0;i<res_73.length;i++){
+    					drawRect(res_73[i],i);
+    				}
+    				//boundingBoxes.fabricCanvas().renderAll.bind(boundingBoxes.fabricCanvas());
+    				/*
 					viewer.addTiledImage({
 						tileSource: mask_url,
 						x: 0,
@@ -315,6 +147,7 @@ App = {
 						opacity: 0.5,
 						index: 1
 					})
+					*/
 				}
 				else{
 					$('#toggle-overlay').html('There is no Prediction Mask');
@@ -322,7 +155,7 @@ App = {
 					$('#toggle-overlay').addClass('uk-button-secondary');
 				}							
 			}else{
-				viewer.world.removeItem(viewer.world.getItemAt(1));;
+				//viewer.world.removeItem(viewer.world.getItemAt(1));;
 				$('#toggle-overlay').html('Display Prediction Mask');
 				$('#toggle-overlay').removeClass('uk-button-primary');
 				$('#toggle-overlay').removeClass('uk-button-secondary');
@@ -350,8 +183,7 @@ App = {
 		for(var i=1;i<=33;i++){
 		    $('#patient_'+i).click(createCallback(i));
 		}
-	
-		
+
 	}
 
 }
